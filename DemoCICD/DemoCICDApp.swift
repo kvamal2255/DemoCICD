15f7afc684bb1587b1c7b47cfb971bb2ba17268c
Amal kv <amalkv@zooservers.com>
2024-08-01 15:06:18 +0530

    Initial Commit

---
+++ DemoCICD/DemoCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCICDApp.swift
+//  DemoCICD
+//
+//  Created by Amal kv on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
